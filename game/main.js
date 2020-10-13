8037261a7fb9fdf55a7d63a050b440cb9bba78ac
LeandroPy <leandrosebold16@gmail.com>
2020-10-13 23:38:24 +0000

    Implementa possibilidade de jogar em duas pessoas

--- game/main.js
+++ game/main.js
@@ -5,22 +5,34 @@ const soundTrack = new Audio('./sounds/soundTrack.mp3');
 const racketShockSoundWithCircle = new Audio('./sounds/racketCollision.mp3');
 const scoreSound = new Audio('./sounds/scoreSound.mp3');
 
-let meusPontos = 0;
-let outrosPontos = 0;
+let leftRacket = {
+    x: 5,
+    y: 150,
+    speed: 15,
+    points: 0
+}
+
+let rightRacket;
+
+let otherPlayer = {
+    x: 585,
+    y: 150,
+    speed: 15,
+    points: 0
+}
+
+let computer = {
+    x: 585,
+    y: 150,
+    speed: 0,
+    points: 0
+}
 
 let circleSpeedForX = 6;
 let circleSpeedForY = 6;
 let xCircle = 300;
 let yCircle = 200;
 let radiusCircle = 15;
-
-let xRacketLeft = 5;
-let yRacketLeft = 150;
-let rackeLeftSpeed = 15;
-
-let xRacketRight = 585;
-let yRacketRight = 150;
-let rackeRightSpeed;
 
 let widthRacket = 10;
 let heightRacket = 90;
@@ -34,14 +46,28 @@ function drawCircle(xCircle, yCircle, radius, color) {
 
 function drawScoreboard() {
     ctx.fillStyle = 'white';
-    ctx.fillText(meusPontos, 278, 26);
-    ctx.fillText(outrosPontos, 321, 26);
+    ctx.fillText(leftRacket.points, 278, 26);
+    ctx.fillText(rightRacket.points, 321, 26);
 }
 
-function clearScreen() {
+function drawBlock() {
     ctx.clearRect(0, 0, 600, 400);
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, 600, 400);
+
+    drawMidField()
+
+}
+
+function drawMidField() {
+    drawRect(300, 5, 10, 40, 'white');
+    drawRect(300, 55, 10, 40, 'white');
+    drawRect(300, 105, 10, 40, 'white');
+    drawRect(300, 155, 10, 40, 'white');
+    drawRect(300, 205, 10, 40, 'white');
+    drawRect(300, 255, 10, 40, 'white');
+    drawRect(300, 305, 10, 40, 'white');
+    drawRect(300, 355, 10, 40, 'white');
 }
 
 function checkCollisionOfCircleWithTheWall() {
@@ -50,16 +76,16 @@ function checkCollisionOfCircleWithTheWall() {
 }
 
 function checkColisionOfCircleWithTheRacket() {
-    if (xCircle - radiusCircle < xRacketLeft + widthRacket
-        && yCircle - radiusCircle < yRacketLeft + heightRacket
-        && yCircle + radiusCircle > yRacketLeft) {
+    if (xCircle - radiusCircle <= leftRacket.x + widthRacket
+        && yCircle - radiusCircle <= leftRacket.y + heightRacket
+        && yCircle + radiusCircle >= leftRacket.y) {
         circleSpeedForX *= -1;
         racketShockSoundWithCircle.play();
     }
 
-    if (xCircle + radiusCircle > xRacketRight - widthRacket
-        && yCircle + radiusCircle > yRacketRight
-        && yCircle - radiusCircle < yRacketRight + heightRacket) {
+    if (xCircle >= rightRacket.x - widthRacket
+        && yCircle + radiusCircle > rightRacket.y
+        && yCircle - radiusCircle < rightRacket.y + heightRacket) {
         circleSpeedForX *= -1
         racketShockSoundWithCircle.play();
     }
@@ -68,10 +94,10 @@ function checkColisionOfCircleWithTheRacket() {
 
 function checksIfPointHappened() {
     if (xCircle > 585) {
-        meusPontos += 1;
+        leftRacket.points += 1;
         scoreSound.play();
     } else if (xCircle < 15) {
-        outrosPontos += 1;
+        rightRacket.points += 1;
         scoreSound.play();
     }
 }
@@ -86,25 +112,30 @@ function moveCircle() {
 }
 
 function drawRacket(xRacket, yRacket, widthRacket, heightRacket, color) {
-    ctx.fillStyle = color;
-    ctx.fillRect(xRacket, yRacket, widthRacket, heightRacket);
+    drawRect(xRacket, yRacket, widthRacket, heightRacket, color)
 }
 
+function drawRect(x, y, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+}
+
+
 function moveRacketRight() {
-    rackeRightSpeed = yCircle - yRacketRight - widthRacket / 2 - 30;
-    yRacketRight += rackeRightSpeed;
+    rackeRightSpeed = yCircle - rightRacket.y - widthRacket / 2;
+    rightRacket.y += rackeRightSpeed;
 }
 
 function screenUpdate() {
-    clearScreen();
+    drawBlock();
 
-    drawRacket(xRacketLeft, yRacketLeft, widthRacket, heightRacket, 'white');
-    drawRacket(xRacketRight, yRacketRight, widthRacket, heightRacket, 'white');
+    drawRacket(leftRacket.x, leftRacket.y, widthRacket, heightRacket, 'white');
+    drawRacket(rightRacket.x, rightRacket.y, widthRacket, heightRacket, 'white');
     drawCircle(xCircle, yCircle, radiusCircle, 'white');
     drawScoreboard();
 
     moveCircle()
-    moveRacketRight();
+    if (rightRacket === computer) moveRacketRight();
 
     requestAnimationFrame(screenUpdate);
 }
@@ -113,10 +144,16 @@ function moveRacket(keyPressed) {
 
     const acceptedMoves = {
         ArrowUp() {
-            if ((yRacketLeft - rackeLeftSpeed) > 0) { yRacketLeft -= rackeLeftSpeed; }
+            if ((leftRacket.y - leftRacket.speed) > 0) leftRacket.y -= leftRacket.speed;
         },
         ArrowDown() {
-            if ((yRacketLeft + heightRacket + rackeLeftSpeed) < canvas.height) { yRacketLeft += rackeLeftSpeed; }
+            if ((leftRacket.y + heightRacket + leftRacket.speed) < canvas.height) leftRacket.y += leftRacket.speed;
+        },
+        w() {
+            if (rightRacket === otherPlayer && ((rightRacket.y - rightRacket.speed) > 0)) rightRacket.y -= rightRacket.speed
+        },
+        s() {
+            if (rightRacket === otherPlayer && ((rightRacket.y + heightRacket + rightRacket.speed) < canvas.height)) rightRacket.y += rightRacket.speed;
         }
     }
 
@@ -137,18 +174,50 @@ function keyboardListener() {
     })
 }
 
-(function init() {
-
+function init() {
+    resetGame();
     initSoundTrack()
 
     canvas.width = 600;
     canvas.height = 400;
 
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, 600, 400);
-
     screenUpdate();
 
     keyboardListener()
 
-})()
+}
+
+function chooseComputerToSecondPlayer() {
+    rightRacket = computer;
+    init();
+}
+
+function chooseOtherPlayerToSecondPlayer() {
+    rightRacket = otherPlayer;
+    init();
+}
+
+function resetGame() {
+    leftRacket = {
+        x: 5,
+        y: 150,
+        speed: 15,
+        points: 0
+    }
+    xCircle = 300;
+    yCircle = 200;
+    circleSpeedForX = 1;
+    circleSpeedForY = 1;
+
+    computer.x = 585
+    computer.y = 150
+    computer.speed = 0
+    computer.points = 0
+
+    otherPlayer.x = 585
+    otherPlayer.y = 150
+    otherPlayer.speed = 15
+    otherPlayer.points = 0
+
+    rightRacket = rightRacket
+}
